9972f7844c77a530e0bb6eb28e67a912d2f30e4f
Nathan Everitt <neveritt@usglobalmail.com>
2013-12-13 12:47:19 -0600

    Made the Postal class not discard important data from its options() method.

--- postal/postal.py
+++ postal/postal.py
@@ -15,5 +15,14 @@ class Postal:
 
     def options(self, package):
         for carrier in self.carriers.values():
-            for service in carrier.get_services(package):
-                yield service
+            for service, data in carrier.get_services(package).iteritems():
+
+                # copy before modifying in case a carrier binding does something odd
+                result = dict(data)
+
+                if 'service' in result:
+                    ### Don't silently overwrite something that might be important
+                    raise Exception()
+
+                result['service'] = service
+                yield result
