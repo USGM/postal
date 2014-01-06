43907ae8829ce2139658b9651de41c27bff9dc18
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2014-01-06 12:41:43 -0600

    USPS now gets domestic and international rates with insurance.

--- postal/tests/test_usps.py
+++ postal/tests/test_usps.py
@@ -1,10 +1,15 @@
 import unittest
+from unittest import SkipTest
 from base import TestCarrier
 from ..carriers.usps import USPSApi
 
 
 class TestUSPS(TestCarrier, unittest.TestCase):
     carrier_class = USPSApi
+
+    def test_international_delayed_shipment(self):
+        raise SkipTest(
+            "USPS does not support delayed international shipments.")
 
 if __name__ == '__main__':
     unittest.main()
