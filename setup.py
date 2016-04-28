f5d3393343f4faf4e556c6851eaccdac2e91a259
ashwini-silicus <ashwini.kaklij@silicus.com>
2016-04-28 20:22:53 +0530

    Updated versions of pillow and reportlab module
    Revised version of postal repository

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_data_files():
 
 setup(
     name='Postal',
-    version='0.1.0',
+    version='0.1.1',
     author='US Global Mail with Silicus Technologies',
     author_email='it@usglobalmail.com',
     url='http://www.usglobalmail.com/',
