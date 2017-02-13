8f4c3fff7d4df80a3ea22731f391f6783b1c48b6
Jonathan Piacenti <jonathan.piacenti@silicus.com>
2017-02-13 07:32:53 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_data_files():
 
 setup(
     name='Postal',
-    version='0.3.5',
+    version='0.3.6',
     author='US Global Mail with Silicus Technologies',
     author_email='it@usglobalmail.com',
     url='http://www.usglobalmail.com/',
