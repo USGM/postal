e0bb83697994e2d7cc6564de6aa20b54a3fd2982
Stepan Shamaiev <skippdot@gmail.com>
2018-01-19 16:09:46 +0200

    Bump Postal version in setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_data_files():
 
 setup(
     name='Postal',
-    version='0.3.15',
+    version='0.3.16',
     author='US Global Mail with Silicus Technologies',
     author_email='it@usglobalmail.com',
     url='http://www.usglobalmail.com/',
